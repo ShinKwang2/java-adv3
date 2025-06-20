06552d0af46b48a91e03809b02581308bbcc7ff9
shinkwang <shinkwang@yonsei.ac.kr>
2025-06-21 07:45:40 +0900

    chapter 4: lambda function by java

---
+++ src/lambda/lambda5/map/MapMainV4.java
@@ -0,0 +1,28 @@
+package lambda.lambda5.map;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class MapMainV4 {
+
+    public static void main(String[] args) {
+        List<String> list = List.of("1", "12", "123", "1234");
+
+        // 문자열을 숫자로 변환
+        List<Integer> numbers = StringToIntegerMapper.map(list, s -> Integer.valueOf(s));
+        System.out.println("numbers = " + numbers);
+
+        // 문자열의길이로 변환
+        List<Integer> lengths = StringToIntegerMapper.map(list, s -> s.length());
+        System.out.println("lengths = " + lengths);
+    }
+
+    private static List<Integer> map(List<String> list, Function<String, Integer> mapper) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String s : list) {
+            numbers.add(mapper.apply(s));
+        }
+        return numbers;
+    }
+}
