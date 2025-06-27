7e47e7f9cc5845f616cde0606c6d352db4cc7a02
shinkwang <shinkwang@yonsei.ac.kr>
2025-06-28 07:56:21 +0900

    chapter 13: Parallel stream

---
+++ src/parallel/forkjoin/CompletableFutureMain.java
@@ -0,0 +1,22 @@
+package parallel.forkjoin;
+
+import util.MyLogger;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static util.MyLogger.*;
+
+public class CompletableFutureMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        CompletableFuture.runAsync(() -> log("Fork/Join"));
+
+        ExecutorService es = Executors.newFixedThreadPool(100);
+        CompletableFuture.runAsync(() -> log("Custom Pool"), es);
+
+        Thread.sleep(1000);
+        es.close();
+    }
+}
